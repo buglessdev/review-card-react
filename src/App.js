23468cb95df52752e0b4fa3d1ab2cc677b2598be
User One <userone@pop-os.localdomain>
2022-08-26 09:28:09 +0530

    refactored

--- src/App.js
+++ src/App.js
@@ -3,44 +3,52 @@ import Header from './components/Header.jsx'
 import ReviewItem from './components/ReviewItem'
 import ReviewStatus from './components/ReviewStatus.jsx';
 
-function App(){
-    
-   
-    const [reviewItems,setReviewItems] = useState(
+function App() {
+    const [reviewItems, setReviewItems] = useState(
         [
-            {'id':1,
-    'text':'Great Products',rating:5},
-    {'id':2,
-    'text':'Cool Stuff',rating:4},
-    {'id':3,
-    'text':'Not bad',rating:3}
-]
-);
+            {
+                'id': 1,
+                'text': 'Great Products', rating: 5
+            },
+            {
+                'id': 2,
+                'text': 'Cool Stuff', rating: 4
+            },
+            {
+                'id': 3,
+                'text': 'Not bad', rating: 3
+            }
+        ]
+    );
 
-    
-    function getAverageRating (){
-        let totalRating=0;
-        reviewItems.forEach((item)=>{
+
+    function getAverageRating() {
+        let totalRating = 0;
+        reviewItems.forEach((item) => {
             totalRating = totalRating + item['rating'];
-            
-        }) 
-        return totalRating/reviewItems.length;
+
+        })
+        console.log(totalRating);
+                console.log(totalRating);
+        
+        return totalRating / reviewItems.length;
     }
 
-    const [averageRating,setAverageRating] = useState(getAverageRating());
+    const [averageRating, setAverageRating] = useState(getAverageRating());
     function removeItem(id) {
-        setReviewItems(reviewItems.filter((item)=>item.id!==id))
+        setReviewItems(reviewItems.filter((item) => item.id !== id))
         setAverageRating(getAverageRating())
+        console.log(getAverageRating());
     }
     return (
         <>
-        <div className="container">
-        <ReviewStatus averageRating = {averageRating}/>
-        {reviewItems.map((item)=><ReviewItem item={item} handleDelete={(id)=>removeItem(id)} />)}
-        </div>
-        
-        
-       </>
+            <div className="container">
+                <ReviewStatus averageRating={averageRating} />
+                {reviewItems.map((item) => <ReviewItem item={item} handleDelete={(id) => removeItem(id)} />)}
+            </div>
+
+
+        </>
 
     )
 }
